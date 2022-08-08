805e35a9792de31e863c5cc672c3564bfc48edc3
Pride Chamisa <55308392+quintofsteel@users.noreply.github.com>
2022-08-08 04:04:40 +0000

    Update the classifier

--- GaussianNB deployment on terrain data/ClassifyNB.py
+++ GaussianNB deployment on terrain data/ClassifyNB.py
@@ -1,14 +1,11 @@
 def classify(features_train, labels_train):   
-    ### import the sklearn module for GaussianNB
-    ### create classifier
-    ### fit the classifier on the training features and labels
-    ### return the fit classifier
+    """
+    Function takes in the features and labels for the training data and returns the trained classifier
+    """
     
-    
-    ### your code goes here!
-    from sklearn.naive_bayes import GaussianNB
-    clf = GaussianNB()
-    clf.fit(features_train, labels_train)
-    return clf
+    from sklearn.naive_bayes import GaussianNB              ### import the sklearn module for GaussianNB
+    clf = GaussianNB()                                      ### create classifier
+    clf.fit(features_train, labels_train)                   ### fit the classifier on the training features and labels
+    return clf                                              ### return the fit classifier
     
     
